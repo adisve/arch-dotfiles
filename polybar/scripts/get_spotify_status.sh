bc507867f4574fa6a0320177a969d693c641d855
Adis Veletanlic <adis.veletanlic@gmail.com>
2022-08-11 16:14:33 +0200

    Edit polybar and i3 configs

--- polybar/scripts/get_spotify_status.sh
+++ polybar/scripts/get_spotify_status.sh
@@ -54,13 +54,13 @@ if [ "$1" == "--status" ]; then
     echo "$STATUS"
 else
     if [ "$STATUS" = "Stopped" ]; then
-        echo "No music is playing"
+        echo "So quiet.."
         # switch_players
     elif [ "$STATUS" = "Paused"  ]; then
         update_hooks "$PARENT_BAR_PID" 2
         playerctl --player=$PLAYER metadata --format "$FORMAT"
     elif [ "$STATUS" = "No player is running"  ]; then
-        echo "$STATUS"
+        echo "So quiet.."
     else
         update_hooks "$PARENT_BAR_PID" 1
         playerctl --player=$PLAYER metadata --format "$FORMAT"
