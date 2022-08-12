00e1f9aa70855e45d62af2b420fd161c2c21f79d
Adis Veletanlic <adis.veletanlic@gmail.com>
2022-08-12 11:56:01 +0200

    Edit i3 config and remove margins between prefix and labels

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,14 +1,10 @@
-
 #!/usr/bin/env sh
 
 CONFIG_LOCATION=/home/"$USER"/.config/polybar/config.ini
-
-# Terminate already running bar instances
+BAR=base
 killall -q polybar
 
-# Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
-# Launch
-polybar --reload "base" -q -c "$CONFIG_LOCATION" &
+polybar --reload "$BAR" -q -c "$CONFIG_LOCATION" &
 
 echo "Bar launched..."
